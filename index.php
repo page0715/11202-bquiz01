fb4f21351985d0709b6b0e4baa9b97773753511b
unknown <page0715@gmail.com>
2023-12-18 13:58:54 +0800

    完成管理者登入登出功能

--- index.php
+++ index.php
@@ -26,8 +26,36 @@
              	<div id="lf" style="float:left;">
             		<div id="menuput" class="dbor">
                     <!--主選單放此-->
-                    	                            <span class="t botli">主選單區</span>
-                                                </div>
+						<span class="t botli">主選單區</span>
+						<?php
+						$mainmu=$Menu->all(['sh'=>1,'menu_id'=>0]);
+						foreach($mainmu as $main){
+						?>
+						<div class='mainmu'>
+							<a  href="<?=$main['href'];?>" style="color:#000; font-size:13px; text-decoration:none;"><?=$main['text'];?></a>
+							<?php
+							
+							if($Menu->count(['menu_id'=>$main['id']])>0){
+								echo "<div class='mw'>";
+								$subs=$Menu->all(['menu_id'=>$main['id']]);
+								foreach($subs as $sub){
+									echo "<a href='{$sub['href']}'>";
+									echo "<div class='mainmu2'>";
+									echo $sub['text'];
+									echo "</div>";
+									echo "</a>";
+								}
+								echo "</div>";
+							}
+							?>
+							
+						</div>
+							
+						</a>
+						<?php
+						}
+						?>
+					</div>
                     <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     	<span class="t">進站總人數 : <?=$Total->find(1)['total'];?></span>
                     </div>
